02cf22e44ebef5ef12463c6c49f4a8e18e8e2088
kasunben <mailtokasun@gmail.com>
2025-11-12 23:29:17 +0100

    minor fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App.jsx";
+import Flow from "./Flow.jsx";
 
 createRoot(document.getElementById("plugin-root")).render(
   <StrictMode>
-    <App />
+    <Flow />
   </StrictMode>
 );
